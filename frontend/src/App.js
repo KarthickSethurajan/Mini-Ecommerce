440e546635604557b321847cad8cc69322131443
Karthick <ss.karthick61@gmail.com>
2025-06-26 19:46:31 +0530

    first version of mini ecommerce

---
+++ frontend/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+
+import Home from './pages/Home';
+
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+import ProductDetail from './pages/ProductDetail';
+import { useState } from 'react';
+import{ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import Cart from './pages/Cart';
+
+
+
+
+
+
+
+
+function App() {
+  const [cartItems,setCartItems]=useState([]);
+  return (
+    <div className="App">
+      
+       
+      <BrowserRouter>
+      <div ><ToastContainer theme='dark'position='top-center'/>
+          <Header cartItems={cartItems}/>
+          <Routes>
+                  
+               <Route path="/"  element={ <Home />}/>
+              <Route path="/search"  element={ <Home />}/>
+               <Route path="/product/:id"  element={ <ProductDetail cartItems={cartItems} setCartItems={setCartItems}/>}/>
+               <Route path="/cart"  element={ <Cart  cartItems={cartItems} setCartItems={setCartItems}/>}/>
+          </Routes>
+            
+      
+      </div>
+       
+      </BrowserRouter>
+      <Footer/>
+   
+    </div>
+  );
+}
+
+export default App;
